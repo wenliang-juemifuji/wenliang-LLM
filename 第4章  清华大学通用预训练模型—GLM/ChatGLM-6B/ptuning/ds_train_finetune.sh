1f54f980c2fa2c110b397abcd5d985b174019913
juemifuji <348722420@qq.com>
2024-11-04 14:39:04 +0800

    Update ds_train_finetune.sh

--- 第4章  清华大学通用预训练模型—GLM/ChatGLM-6B/ptuning/ds_train_finetune.sh
+++ 第4章  清华大学通用预训练模型—GLM/ChatGLM-6B/ptuning/ds_train_finetune.sh
@@ -1,26 +1,24 @@
 
-LR=1e-4
-
-MASTER_PORT=$(shuf -n 1 -i 10000-65535)
-
-deepspeed --num_gpus=4 --master_port $MASTER_PORT main.py \
+LR=1e-5
+MASTER_PORT=$(shuf -n 1 -I 10000-65535)
+deepspeed —num_gpus=8 —master_port $MASTER_PORT main.py \
     --deepspeed deepspeed.json \
     --do_train \
-    --train_file AdvertiseGen/train.json \
-    --test_file AdvertiseGen/dev.json \
+    --preprocessing_num_workers 32 \
+    --train_file data/train.json \
+    --test_file data/dev.json \
     --prompt_column content \
     --response_column summary \
-    --overwrite_cache \
+    --cache_dir cache/batch16 \
     --model_name_or_path THUDM/chatglm-6b \
-    --output_dir ./output/adgen-chatglm-6b-ft-$LR \
+    --output_dir ./model/adgen-chatglm-6b-ft \
     --overwrite_output_dir \
-    --max_source_length 64 \
-    --max_target_length 64 \
-    --per_device_train_batch_size 4 \
+    --max_source_length 512 \
+    --max_target_length 512 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
     --predict_with_generate \
-    --max_steps 5000 \
     --logging_steps 10 \
     --save_steps 1000 \
     --learning_rate $LR \
